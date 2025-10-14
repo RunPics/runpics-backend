2db698c50d7ca1dbc59ea1d4a13d28e4db756073
이상원 <sangwon020214@gmail.com>
2025-10-14 21:46:29 +0900

    start: 스프링부트 초기 세팅

---
+++ src/main/java/com/runpics/runpics_backend/RunpicsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.runpics.runpics_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunpicsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunpicsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runpics/runpics_backend/RunpicsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runpics.runpics_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunpicsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
